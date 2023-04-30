82ae162be958984eadc4a7a768b50bc12516c023
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-04-30 15:02:27 +0200

    First commit. Add buscador component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
+})
+
+export class BuscadorComponent {
+  sendData(event: any){
+    console.log(event.target.value)
+  }
+}
