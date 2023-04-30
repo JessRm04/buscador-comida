9f31c85d5eb19801458f42f96cfaa78daa033138
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-04-30 16:31:25 +0200

    add constructor, search function

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-buscador',
@@ -7,7 +8,23 @@ import { Component } from '@angular/core';
 })
 
 export class BuscadorComponent {
-  sendData(event: any){
-    console.log(event.target.value)
+
+  beer: any;
+
+  constructor(private http: HttpClient) {}
+
+  search(query: string) {
+    console.log('Función search llamada con el valor:', query);
+    this.http.get(`https://api.punkapi.com/v2/beers?food=${query}`).subscribe(data => {
+      console.log('Respuesta de la API:', data);
+      this.beer = data;
+    });
+  }
+
+  sendData(event: KeyboardEvent) {
+    const query = (event.target as HTMLInputElement).value;
+    console.log('Valor del input:', query);
+    this.search(query);
+
   }
 }
