2a0b6b1aedb12729b060f39993f214465ee98dce
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-04-30 16:41:04 +0200

    change section search-results, so it only shows the starter

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -14,16 +14,13 @@ export class BuscadorComponent {
   constructor(private http: HttpClient) {}
 
   search(query: string) {
-    console.log('Función search llamada con el valor:', query);
     this.http.get(`https://api.punkapi.com/v2/beers?food=${query}`).subscribe(data => {
-      console.log('Respuesta de la API:', data);
       this.beer = data;
     });
   }
 
   sendData(event: KeyboardEvent) {
     const query = (event.target as HTMLInputElement).value;
-    console.log('Valor del input:', query);
     this.search(query);
 
   }
